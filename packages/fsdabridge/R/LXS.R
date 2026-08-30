614c60322bff6942a33e5fcff00bf2b1efbb1a18
Shahzad Ali <shahzad.ali@studenti.unipr.it>
2026-08-30 17:48:42 +0200

    Fix LXS signature to accept y and X and update live tests

--- packages/fsdabridge/R/LXS.R
+++ packages/fsdabridge/R/LXS.R
@@ -1,10 +1,12 @@
-#' @title LXS Wrapper
-#' @description MATLAB LXS function wrapper.
+#' LXS
+#'
+#' @description Computes least trimmed squares (LXS) estimators.
 #' @param handle MATLAB engine handle.
-#' @param Y Input data.
-#' @param ... Additional arguments.
-#' @return Returns the MATLAB output.
+#' @param y Response variable vector.
+#' @param X Predictor matrix.
+#' @param ... Additional arguments passed to MATLAB.
+#' @return MATLAB engine output for LXS.
 #' @export
-LXS <- function(handle, Y, ...) {
-  fsda_call(handle, "LXS", Y, ...)
+LXS <- function(handle, y, X, ...) {
+  fsda_call(handle, "LXS", y, X, ...)
 }
